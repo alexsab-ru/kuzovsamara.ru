bf3e5ad7550fe8d85f0e15fbf1a7795b6c1ad131
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-09-28 16:32:09 +0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+import colors from 'tailwindcss/colors'
+export const content = ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}']
+export const theme = {
+  container: {
+    center: true,
+    padding: '1.25rem',
+  },
+  // colors: {},
+  extend: {
+    // fontFamily: {
+    //   'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
+    // },
+  },
+}
+export const plugins = []
