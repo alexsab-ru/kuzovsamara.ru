f68fc02984996df2d869f080691d4a81528fe3c3
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-09-28 16:47:38 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://alexsab-ru.github.io/kuzovsamara.ru/'
 });
