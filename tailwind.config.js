6ea83e74210d92daedf9480caa11628dd573f422
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-10-03 20:12:39 +0500

    update files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 import colors from 'tailwindcss/colors'
+delete colors.lightBlue;
+delete colors.warmGray;
+delete colors.trueGray;
+delete colors.coolGray;
+delete colors.blueGray;
 export const content = ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}']
 export const theme = {
   container: {
