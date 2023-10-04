28b0ae1a5b3b60d062af190616f2f71832903ebd
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-10-04 07:50:34 +0500

    header

--- src/js/data.alpine.js
+++ src/js/data.alpine.js
@@ -1,7 +1,12 @@
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
-  // Optional: Extend Alpine.js
-  // Alpine.directive('foo', ...)
-
-  window.Alpine = Alpine;
-  Alpine.start();
+document.addEventListener("alpine:init", () => {
+	Alpine.data("header", () => ({
+		open: false,
+        init() {
+			document.addEventListener("scroll", (e) => this.open = false);
+		},
+	}));
+});
+window.Alpine = Alpine;
+Alpine.start();
