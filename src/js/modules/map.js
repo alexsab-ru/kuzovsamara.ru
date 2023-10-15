6fe67bb96653bde619a0f5fe409cffcedef26b2a
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-10-15 11:26:13 +0500

    output data using salons.json

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,28 +1,16 @@
+import salons from '@/salons.json';
+import  { phoneFormat } from '@/use/numbers.format';
+const balloonContentHeader = name => {return `<h6 class="text-sm"><b>${name}</b></h6>`};
+const balloonContentBody = (hours, phone, link) => {
+	return `
+	<p>${hours}</p>
+	<a href="tel:${phoneFormat(phone)}" class="inline-block text-lg text-accent-500 my-1">${phone}</a><br>
+	<a href="${link}"  target="_blank" class="text-sm">📍 Как добраться</a><br>
+	`
+}
 const parent = document.querySelector(".map");
 
-let maps = [
-		{
-			position: [53.275197, 50.227754],
-			balloonContentHeader: "<h6><b>Кузов Эксперт</b></h6>",
-			balloonContentBody:
-				'<p>пн — вс: 8:00 — 20:00</p> \
-        <a href="tel:+78463210000" class="text-lg text-accent-500">8 (846) 321-00-00</a><br> \
-        <a href="https://yandex.ru/maps/51/samara/?from=api-maps&ll=50.228953%2C53.274141&mode=routes&origin=jsapi_2_1_79&pt=50.227754%2C53.275197~50.178598%2C53.138397&rtext=~53.274703%2C50.227773&rtt=auto&ruri=~ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1NzM5MDc3OBJJ0KDQvtGB0YHQuNGPLCDQodCw0LzQsNGA0LAsINCU0LXQvNC-0LrRgNCw0YLQuNGH0LXRgdC60LDRjyDRg9C70LjRhtCwLCA1NSIKDTnpSEIVzhlVQg%2C%2C&z=16.89"  target="_blank" class="">📍 Как добраться</a><br>',
-			balloonContentFooter: "",
-			hintContent: "Кузов Эксперт на Демократической",
-		},
-		{
-			position: [53.138397, 50.178598],
-			balloonContentHeader: "<h6><b>Кузов Эксперт</b></h6>",
-			balloonContentBody:
-				'<p>пн — вс: 8:00 — 20:00</p> \
-        <a href="tel:+78463210909" class="text-lg text-accent-500">8 (846) 321-09-09</a><br> \
-        <a href="https://yandex.ru/maps/51/samara/?from=api-maps&ll=50.178605%2C53.138478&mode=routes&origin=jsapi_2_1_79&pt=50.178598%2C53.138397~50.227754%2C53.275197&rtext=~53.138325%2C50.178537&rtt=auto&ruri=~ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1NzM1NjM0NxI40KDQvtGB0YHQuNGPLCDQodCw0LzQsNGA0LAsINCu0LbQvdC-0LUg0YjQvtGB0YHQtSwgMTLRgTMiCg3itkhCFbeNVEI%2C&z=18.45"  target="_blank" class="">📍 Как добраться</a><br>',
-			balloonContentFooter: "",
-			hintContent: "Кузов Эксперт на Южном",
-		},
-	],
-	start_load_script = false, // Переменная для определения была ли хоть раз загружена Яндекс.Карта (чтобы избежать повторной загрузки при наведении)
+let start_load_script = false, // Переменная для определения была ли хоть раз загружена Яндекс.Карта (чтобы избежать повторной загрузки при наведении)
 	end_load_script = false; // Переменная для определения был ли загружен скрипт Яндекс.Карт полностью (чтобы не возникли какие-нибудь ошибки, если мы загружаем несколько карт одновременно)
 
 let myMapTemp = null;
@@ -36,15 +24,16 @@ function init() {
 		});
 	}
 	myMapTemp.behaviors.disable("scrollZoom");
-	maps.map((map) => {
+	salons.map((salon) => {
+		console.log(salon.map.coords);
 		myMapTemp.geoObjects.add(
 			new ymaps.Placemark(
-				map.position,
+				[salon.map.coords[0], salon.map.coords[1]],
 				{
-					balloonContentHeader: map.balloonContentHeader,
-					balloonContentBody: map.balloonContentBody,
-					balloonContentFooter: map.balloonContentFooter,
-					hintContent: map.hintContent,
+					balloonContentHeader: balloonContentHeader(salon.name),
+					balloonContentBody: balloonContentBody(salon.work_hours, salon.phone, salon.map.ya_link),
+					balloonContentFooter: '',
+					hintContent: salon.address,
 				},
 				{
 					preset: "islands#blueAutoIcon",
