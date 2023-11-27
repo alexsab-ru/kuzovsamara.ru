e3fede3c4fdeec328f64d8b4f72fc98da09bcb93
Alexander Sabanov <alexsab@mail.ru>
2023-11-27 15:49:57 +0400

    upd legal info

--- src/const.js
+++ src/const.js
@@ -1,2 +1,4 @@
 export const SITE_NAME = 'Кузов Эксперт'; //Название сайта по умолчанию
+export const LEGAL_ENTITY = 'ООО «Эксперт Авто Самара»'; // Юр лицо
+export const LEGAL_INN = '6312147970'; // Юр лицо
 export const SITE_DESCR = 'Первый официальный кузовной по&nbsp;автомобилям из&nbsp;Китая'; //Описание сайта по умолчанию
